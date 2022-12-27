f153617480e79dc730f283b304a65a9399343436
saradeveloper <113747290+saradeveloper@users.noreply.github.com>
2022-12-27 22:31:42 +0900

    improve JavaScript code2

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const items = document.querySelector('.items');
+const form = document.querySelector('.new-form');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  onAdd();
+});
 
 function onAdd() {
   // 1. 사용자가 입력한 텍스트를 받아옴
@@ -36,15 +42,18 @@ function createItem(text) {
   return itemRow;
 }
 
-addBtn.addEventListener('click', () => {
-  onAdd();
-});
+// addBtn.addEventListener('click', () => {
+//   onAdd();
+// });
 
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    onAdd();
-  }
-});
+// input.addEventListener('keydown', (event) => {
+//   if (event.isComposing) {
+//     return;
+//   }
+//   if (event.key === 'Enter') {
+//     onAdd();
+//   }
+// });
 
 items.addEventListener('click', (event) => {
   const id = event.target.dataset.id;
